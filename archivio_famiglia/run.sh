4266cdaed28daf5dd694ec9ffc1157e96452adab
simone-losito <121686360+simone-losito@users.noreply.github.com>
2026-04-25 18:25:01 +0200

    Copy options.json to addon_options.json if exists
    Added functionality to copy options from options.json to addon_options.json with appropriate permissions.

--- archivio_famiglia/run.sh
+++ archivio_famiglia/run.sh
@@ -3,23 +3,24 @@ set -e
 
 echo "Starting Archivio Famiglia..."
 
-# cartelle
 mkdir -p /share/archivio
 mkdir -p /var/www/html/backups
 
-# symlink upload
 if [ ! -L /var/www/html/uploads ]; then
     rm -rf /var/www/html/uploads
     ln -s /share/archivio /var/www/html/uploads
 fi
 
-# permessi
+# Copio opzioni add-on in un punto leggibile da PHP
+if [ -f /data/options.json ]; then
+    cp /data/options.json /var/www/html/config/addon_options.json
+    chmod 644 /var/www/html/config/addon_options.json
+fi
+
 chown -R www-data:www-data /var/www/html
 chown -R www-data:www-data /share/archivio
 
-# DEBUG
 echo "Contenuto /var/www/html:"
 ls -la /var/www/html
 
-# AVVIO APACHE
 exec apache2-foreground
