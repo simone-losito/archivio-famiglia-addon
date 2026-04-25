269a0839355c493b85fb41cbbe4bd2475f943f03
simone-losito <121686360+simone-losito@users.noreply.github.com>
2026-04-25 18:10:40 +0200

    Refactor database connection to use options file

--- archivio_famiglia/rootfs/var/www/html/config/database.php
+++ archivio_famiglia/rootfs/var/www/html/config/database.php
@@ -1,12 +1,28 @@
 <?php
-$DB_HOST = 'homeassistant';
-$DB_NAME = 'homeassistant';
-$DB_USER = 'homeassistant';
-$DB_PASS = 'la_tua_password';
-$DB_PORT = 3306;
+// config/database.php
 
-$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT);
-if ($conn->connect_error) {
-    die("Errore DB: " . $conn->connect_error);
+$optionsFile = '/data/options.json';
+
+$dbHost = 'core-mariadb';
+$dbName = 'homeassistant';
+$dbUser = 'homeassistant';
+$dbPass = '';
+
+if (is_file($optionsFile)) {
+    $options = json_decode(file_get_contents($optionsFile), true);
+
+    if (is_array($options)) {
+        $dbHost = $options['db_host'] ?? $dbHost;
+        $dbName = $options['db_name'] ?? $dbName;
+        $dbUser = $options['db_user'] ?? $dbUser;
+        $dbPass = $options['db_pass'] ?? $dbPass;
+    }
 }
+
+$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName, 3306);
+
+if ($conn->connect_error) {
+    die("Errore connessione database: " . $conn->connect_error);
+}
+
 $conn->set_charset("utf8mb4");
